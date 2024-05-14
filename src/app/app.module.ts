6e6601d99bbd8769d7d36122ea5cf6256afddeba
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-05-14 10:41:44 -0600

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,57 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { ToastrModule } from 'ngx-toastr';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      progressBar: true,
+      progressAnimation: 'increasing',
+    }),
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private token = '';
+  private jwtToken$ = new BehaviorSubject<string>(this.token);
+  private API_URL = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient,
+              private router: Router,
+              private toast: ToastrService) {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const fetchedToken = localStorage.getItem('act');
+      if (fetchedToken) {
+        this.token = atob(fetchedToken);
+        this.jwtToken$.next(this.token);
+      }
+    }
+  }
+
+  get jwtUserToken(): Observable<string> {
+    return this.jwtToken$.asObservable();
+  }
+
+  /* Getting All Todos */
+  getAllTodos(): Observable<any> {
+    return this.http.get(`${this.API_URL}/todos`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    });
+  }
+
+  login(email: string, password: string) {
+    this.http.post<{ token: string }>(`${this.API_URL}/auth/login`, { email, password })
+      .subscribe((res) => {
+        this.token = res.token;
+
+        if (this.token) {
+          this.toast.success('Login successful, redirecting now...', '', {
+            timeOut: 700,
+            positionClass: 'toast-top-center'
+          }).onHidden.toPromise().then(() => {
+            this.jwtToken$.next(this.token);
+            localStorage.setItem('act', btoa(this.token));
+            this.router.navigateByUrl('/').then();
+          });
+        }
+      }, (err: HttpErrorResponse) => {
+        this.toast.error('Login failed', 'Error', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center'
+        });
+        console.log(err.message);
+      });
+  }
+
+  logout() {
+    this.token = '';
+    this.jwtToken$.next(this.token);
+    this.toast.success('Logged out successfully', '', {
+      timeOut: 500
+    }).onHidden.subscribe(() => {
+      localStorage.removeItem('act');
+      this.router.navigateByUrl('/login').then();
+    });
+    return '';
+  }
+
+  createRecipe(title: string, description: string, calories: number, meal: string) {
+    return this.http.post(`${this.API_URL}/todos`, { title, description,calories, meal }, {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    });
+  }
+
+  updateStatus(statusValue: string, todoId: number) {
+    return this.http.patch(`${this.API_URL}/todos/${todoId}`, { status: statusValue }, {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    }).pipe(
+      tap(res => {
+        if (res) {
+          this.toast.success('Status updated successfully', '', {
+            timeOut: 1000
+          });
+        }
+      })
+    );
+  }
+
+  deleteTodo(todoId: number) {
+    return this.http.delete(`${this.API_URL}/todos/${todoId}`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    }).pipe(
+      tap(res => {
+        if (res) {
+          this.toast.success('Todo deleted successfully');
+        }
+      })
+    );
+  }
+}
+
