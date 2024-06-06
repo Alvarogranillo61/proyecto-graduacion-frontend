b64ecb6f6e4f341dfdd825d9b6005854e5f98545
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-06-06 16:52:25 -0600

    production

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  // Otras configuraciones específicas para el entorno de desarrollo
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://lobster-app-6xcst.ondigitalocean.app/api',  // Reemplaza con la URL real de tu backend en producción
+  // Otras configuraciones específicas para el entorno de producción
+};
