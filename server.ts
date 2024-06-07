6df7cb572f87fa909437f5458203241e94285e59
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-06-06 21:21:49 -0600

    ahora sera posible?

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const PORT = process.env['PORT'] || 80;
+const PORT = parseInt(process.env['PORT'] || '8080', 10);
 
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist/proyecto-graduacion-frontend')));
@@ -12,6 +12,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/proyecto-graduacion-frontend/index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
